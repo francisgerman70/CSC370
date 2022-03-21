c0e4b36cf4a02d906a415d18fb9bc302e7401409
francis german <40007172+francisgerman70@users.noreply.github.com>
2022-03-21 16:42:42 -0700

    Add files via upload

--- query04.sql
+++ query04.sql
@@ -4,3 +4,12 @@
 -- 0.8 marks: correct answer
 
 -- Replace this comment line with the actual query
+SELECT
+   *
+FROM
+(SELECT DISTINCT  `Name`, count(`Name`) as `Frequency`
+FROM (SELECT `Name`,`Date` FROM `Badge`
+WHERE `Date` > '2019-12-31') as inn
+GROUP BY `Name`) as innerTable
+ORDER BY `Frequency` DESC
+LIMIT 20;
